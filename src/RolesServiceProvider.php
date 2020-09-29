7aac7162f8e8131e85a99514fff3dcda734c73cf
michael <38669639+michael-ashraf-93@users.noreply.github.com>
2020-09-29 16:35:54 +0200

    repush

--- src/RolesServiceProvider.php
+++ src/RolesServiceProvider.php
@@ -22,16 +22,16 @@ class RolesServiceProvider extends ServiceProvider
         $target = database_path('migrations') . '/';
 
         $stub_controller = __DIR__ . '/Controllers/';
-        $target_controller = app()->path('Http/Controllers');
+//        $target_controller = app()->path('Http/Controllers');
 
         $this->publishes([
             $stub . 'create_roles_table.php' => $target . date('Y_m_d_His', time()) . '_create_roles_table.php',
             $stub . 'create_users_roles_table.php' => $target . date('Y_m_d_His', time() + 1) . '_create_users_roles_table.php'
         ], 'migrations');
 
-        $this->publishes([
-            $stub_controller . 'RolesController.php' => $target_controller . 'RolesController.php',
-        ], 'migrations');
+//        $this->publishes([
+//            $stub_controller . 'RolesController.php' => $target_controller . 'RolesController.php',
+//        ], 'migrations');
 
     }
 
